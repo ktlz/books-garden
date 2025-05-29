fedf5052a20b8e17691a41ffa5c26e2015b052c3
ktlz <katya.llazorenko@gmail.com>
2025-05-29 13:41:43 +0200

    fix imports

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
-import Button from "./Button";
+import Button from "./Button/Button";
 import SplitContent from "./SplitContent";
 
 export { Navbar, Hero, Button, SplitContent };
